b064fdd28a7a08c989257b5c954f3f9f07ab44d5
Russel Fonta <fontawestbrook99@gmail.com>
2026-09-06 15:51:24 +0200

    feat: initialise variable in script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// Initialization of variable
+const button = document.querySelector('.add-btn')
+const Balance = document.querySelector('.balance')
+const Income = document.querySelector('.income')
+const Expense = document.querySelector('.expense')
+const transactionName = document.getElementById('transaction')
+const transactionAmount = document.getElementById('amount')
