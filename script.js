83837a5209d8288a77098a580df0c370802befd6
Russel Fonta <fontawestbrook99@gmail.com>
2026-09-06 18:20:25 +0200

    feat: add function to update income, balance and income each time the function is called

--- script.js
+++ script.js
@@ -4,7 +4,17 @@ const income = document.querySelector('.income')
 const expense = document.querySelector('.expense')
 const transactionName = document.getElementById('transaction')
 const transactionAmount = document.getElementById('amount')
-const transactionList = document.querySelector('.transactionList')
+const transactionList = document.querySelector('.list')
+
+let currentIncome = 0
+let currentExpense = 0
+let currentBalance = 0
+
+const update_transaction = () => {
+    balance.textContent = `$${currentBalance.toFixed(2)}`
+    income.textContent = `$${currentIncome.toFixed(2)}`
+    balance.textContent = `$${currentBalance.toFixed(2)}`
+}
 
 button.addEventListener('click', (e) => {
   e.preventDefault()
