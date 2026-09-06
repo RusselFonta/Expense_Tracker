9c8467f8db8207d9d46da3fb33dfc9629ca3f75f
Russel Fonta <fontawestbrook99@gmail.com>
2026-09-06 17:17:26 +0200

    feat: add new transation function to history with the delete function insert in

--- script.js
+++ script.js
@@ -1,7 +1,35 @@
-// Initialization of variable
 const button = document.querySelector('.add-btn')
-const Balance = document.querySelector('.balance')
-const Income = document.querySelector('.income')
-const Expense = document.querySelector('.expense')
+const balance = document.querySelector('.balance')
+const income = document.querySelector('.income')
+const expense = document.querySelector('.expense')
 const transactionName = document.getElementById('transaction')
 const transactionAmount = document.getElementById('amount')
+const transactionList = document.querySelector('.transactionList')
+
+button.addEventListener('click', (e) => {
+  e.preventDefault()
+
+  const transName = transactionName.value.trim()
+  const transAmount = transactionAmount.value.trim()
+
+  if (transName === '' || transAmount === '') {
+    alert('Enter transaction name and amount to continue')
+    return
+  }
+
+  const newTransaction = document.createElement('li')
+  const deleteTransaction = document.createElement('button')
+  deleteTransaction.textContent = 'X'
+  deleteTransaction.className = 'delete_trans'
+
+  deleteTransaction.addEventListener('click', () => {
+    newTransaction.remove()
+  })
+
+  newTransaction.textContent = `${transName} ${transAmount} `
+  newTransaction.appendChild(deleteTransaction)
+  transactionList.appendChild(newTransaction)
+
+  transactionName.value = ''
+  transactionAmount.value = ''
+})
